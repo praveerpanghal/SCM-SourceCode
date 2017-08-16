92436f4fb9c691351bf6a9fee291d8302460dc5b
poornaui <poorna@ubsols.com>
2017-08-16 18:53:39 +0530

    search code added

--- Src/scripts/controllers/HeaderCtrl.js
+++ Src/scripts/controllers/HeaderCtrl.js
@@ -2,4 +2,10 @@ app.controller("HeaderCtrl" ,function($scope, $location){
 	$scope.$on('$routeChangeStart', function() { 
 		$scope.path = $location.path();   		
  	});
+ 	// search functionality
+ 	$scope.action = function(username){
+        if(username){                
+            $location.path('/friends/'+username);
+        }
+    }
 });

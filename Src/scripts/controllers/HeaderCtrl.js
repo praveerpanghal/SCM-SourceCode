a42f698d469c0aec659c150831dc040e2441b6d5
poornaui <poorna@ubsols.com>
2017-09-14 17:26:17 +0530

    'search page'

--- Src/scripts/controllers/HeaderCtrl.js
+++ Src/scripts/controllers/HeaderCtrl.js
@@ -1,4 +1,7 @@
-app.controller("HeaderCtrl" ,function($scope, $location){
+app.controller("HeaderCtrl" ,function($scope, $location, LS){
+	$scope.keySearch = true;
+	var user = LS.getData();
+		
 	$scope.$on('$routeChangeStart', function() { 
 		$scope.path = $location.path();   		
  	});
@@ -7,5 +10,10 @@ app.controller("HeaderCtrl" ,function($scope, $location){
         if(username){                
             $location.path('/friends/'+username);
         }
-    }
+	}
+	
+	if(user == null && $location.path() == '/pages'){
+		$scope.keySearch = false;
+		console.log($scope.keySearch);
+	}
 });
--- Src/scripts/controllers/PagesCtrl.js
+++ Src/scripts/controllers/PagesCtrl.js
@@ -1,17 +1,15 @@
 app.controller("PagesCtrl", ['$location', '$route', 'ServiceUrls', 'HttpService', 'LS', function($location, $route, ServiceUrls, HttpService, LS){
-	
-	
-	var vm = this;
-    vm.test = "test";
-    $(document).ready(function() {
-       $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
-           e.preventDefault();
-           $(this).siblings('a.active').removeClass("active");
-           $(this).addClass("active");
-           var index = $(this).index();
-           $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
-           $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
-       });
-    });
+      var vm = this;
+      vm.key = false;
+      $(document).ready(function() {
+            $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+                  e.preventDefault();
+                  $(this).siblings('a.active').removeClass("active");
+                  $(this).addClass("active");
+                  var index = $(this).index();
+                  $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+                  $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+            });
+      });
 	
 }]);

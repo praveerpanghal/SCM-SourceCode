cb962ed302682af60fed7d905638ee26a528ef79
praveerpanghal <praveer.panghal11@gmail.com>
2019-01-18 11:01:39 -0500

    adding new api url

--- serviceUrl.js
+++ serviceUrl.js
@@ -1,2 +1,3 @@
 /* Service URL's */
-var BaseUrl = 'http://162.17.231.117:1212/ServiceSCM.svc/';
+//var BaseUrl = 'http://162.17.231.117:1212/ServiceSCM.svc/';
+var BaseUrl = 'http://praveer-001-site4.ctempurl.com/ServiceSCM.svc/';
